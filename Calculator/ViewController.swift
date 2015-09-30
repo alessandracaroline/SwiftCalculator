73f45e5f8d779242f9e70b7259eff85937f55a55
Alessandra Caroline Faisst <caroline.faisst@gmail.com>
2015-09-29 22:56:41 -0700

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Alessandra Caroline Faisst on 29/09/15.
+//  Copyright © 2015 Alessandra Caroline Faisst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
